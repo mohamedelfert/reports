9059e918535908428bb5f86502b96231ccfe6930
Mohamed Ibrahiem Elfert <mohamedelfert@yahoo.com>
2024-05-12 12:53:35 +0300

    Add MeetingsReportAction for handling meetings reports; update ReportEnginSearchController to include new action

--- Modules/ReportEnginSearch/Http/Controllers/ReportEnginSearchController.php
+++ Modules/ReportEnginSearch/Http/Controllers/ReportEnginSearchController.php
@@ -2,16 +2,13 @@
 
 namespace Modules\ReportEnginSearch\Http\Controllers;
 
-use App\User;
-use Exception;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\ReportEnginSearch\Http\Controllers\actions\AgentsReportAction;
 use Modules\ReportEnginSearch\Http\Controllers\actions\MeetingsOverallReportAction;
+use Modules\ReportEnginSearch\Http\Controllers\actions\MeetingsReportAction;
 use Modules\ReportEnginSearch\Http\Controllers\actions\OverAllActivitiesAction;
 use Modules\ReportEnginSearch\Http\Controllers\actions\PerformanceReportAction;
 use Modules\ReportEnginSearch\Http\Requests\ReportRequest;
-use Yajra\DataTables\Facades\DataTables;
 
 class ReportEnginSearchController extends Controller
 {
@@ -19,47 +16,49 @@ class ReportEnginSearchController extends Controller
         OverAllActivitiesAction     $overAllActivitiesAction,
         AgentsReportAction          $agentsReportAction,
         PerformanceReportAction     $performanceReportAction,
-        MeetingsOverallReportAction $meetingsOverallReportAction
+        MeetingsOverallReportAction $meetingsOverallReportAction,
+        MeetingsReportAction        $meetingsReportAction
     )
     {
         $this->overAllActivitiesAction = $overAllActivitiesAction;
         $this->agentsReportAction = $agentsReportAction;
         $this->performanceReportAction = $performanceReportAction;
         $this->meetingsOverallReportAction = $meetingsOverallReportAction;
+        $this->meetingsReportAction = $meetingsReportAction;
     }
 
 
-    public function index(Request $request)
-    {
-        /*
-            1. Angular: they will send api request to laravel (PL: 5, CP: 3)
-            2. Laravel: will query users with (PL: 5, CP: 1)
-            3. Pluck Users IDs and Group IDs
-            4. Send request to PY
-        */
-        // (2) Get Users
-        $pl = $request->input('pl', 5);
-        $cp = $request->input('cp', 1);
-        $skip = $pl * ($cp - 1);
-        $users = User::skip($skip)->take($pl)->get();
-        // (3) Users IDs
-        $users_ids = with(clone $users)->pluck('id')->toArray();
-        $groups_ids = with(clone $users)->pluck('group_id')->toArray();
-        // (4) Python Request: Sub-domain ID, Users IDs, Start and End Dates
-        try {
-            $report_data = collect([]); // Should be replaced with PY Request
-            return Datatables::of($users)
-                ->addColumn('report', function ($user) use ($report_data) { // 100ms
-                    return with(clone $report_data)->filter(function ($user_report) use ($user) {
-                        return $user_report->user_id == $user->id;
-                    });
-                })
-                ->make(true);
-        } catch (Exception $e) {
-            // Handle exceptions (e.g., connection issues, server errors)
-            return response()->json(['error' => $e->getMessage()], 500);
-        }
-    }
+//    public function index(Request $request)
+//    {
+//        /*
+//            1. Angular: they will send api request to laravel (PL: 5, CP: 3)
+//            2. Laravel: will query users with (PL: 5, CP: 1)
+//            3. Pluck Users IDs and Group IDs
+//            4. Send request to PY
+//        */
+//        // (2) Get Users
+//        $pl = $request->input('pl', 5);
+//        $cp = $request->input('cp', 1);
+//        $skip = $pl * ($cp - 1);
+//        $users = User::skip($skip)->take($pl)->get();
+//        // (3) Users IDs
+//        $users_ids = with(clone $users)->pluck('id')->toArray();
+//        $groups_ids = with(clone $users)->pluck('group_id')->toArray();
+//        // (4) Python Request: Sub-domain ID, Users IDs, Start and End Dates
+//        try {
+//            $report_data = collect([]); // Should be replaced with PY Request
+//            return Datatables::of($users)
+//                ->addColumn('report', function ($user) use ($report_data) { // 100ms
+//                    return with(clone $report_data)->filter(function ($user_report) use ($user) {
+//                        return $user_report->user_id == $user->id;
+//                    });
+//                })
+//                ->make(true);
+//        } catch (Exception $e) {
+//            // Handle exceptions (e.g., connection issues, server errors)
+//            return response()->json(['error' => $e->getMessage()], 500);
+//        }
+//    }
 
     public function reports(ReportRequest $request)
     {
@@ -75,6 +74,9 @@ class ReportEnginSearchController extends Controller
                 break;
             case('meetings-overall'):
                 $response = $this->meetingsOverallReportAction->execute($request);
+                break;
+            case('meetings'):
+                $response = $this->meetingsReportAction->execute($request);
                 break;
             default:
                 $response = null;
